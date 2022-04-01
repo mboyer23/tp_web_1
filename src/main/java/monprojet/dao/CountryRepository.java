a605fbd640ebdf1f0ad9e205b65777d0bbfbddea
mboyer23 <101579896+mboyer23@users.noreply.github.com>
2022-04-01 10:47:12 +0200

    correction commit1

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -1,17 +1,17 @@
-package monprojet.dao; 
-// dao = data access object
-import java.util.*;
+package monprojet.dao;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import monprojet.entity.Country;
+import monprojet.dto.PopulationResult;
 import monprojet.entity.City;
-// This will be AUTO IMPLEMENTED by Spring 
-//
+import monprojet.entity.Country;
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
     
-    @Query(value = "SELECT SUM(City.POPULATION) "
+    /*@Query(value = "SELECT SUM(City.POPULATION) "
         + "FROM Country, City "
         + "WHERE Country.ID = :id "
         +" AND Country.ID = City.COUNTRY_ID",
@@ -29,5 +29,24 @@ public interface CountryRepository extends JpaRepository<Country, Integer> {
         + "GROUP BY Country.NAME",
         nativeQuery = true)
     public List<PaysParPopulation> populationDeChaquePays();
-    
+    */
+
+    //CORRECTION
+    // JPQL : formulée sur le modèle conceptuel de données
+    @Query("SELECT SUM(c.population) FROM City c WHERE c.country.id = :idDuPays")
+    int populationDuPaysJPQL(int idDuPays);
+
+    // SQL : formulée sur le modèle logique de données, il faut connaître la clé étrangère
+    @Query(value = "SELECT SUM(c.population) FROM City c WHERE c.country_id = :idDuPays", 
+    nativeQuery = true)
+    int populationDuPaysSQL(int idDuPays);
+
+    // JPQL : formulée sur le modèle conceptuel de données, la jointure est implicite
+    @Query("SELECT c.country.name AS countryName, SUM(c.population) AS populationTotale FROM City c GROUP BY countryName")
+    List<PopulationResult> populationParPaysJPQL();
+
+    // SQL : formulée sur le modèle logique de données, il faut expliciter la jointure
+    @Query(value = "SELECT Country.name AS countryName, SUM(population) AS populationTotale FROM Country INNER JOIN City ON country_id = Country.id GROUP BY countryName", 
+    nativeQuery = true)
+    List<PopulationResult> populationParPaysSQL();
 }
---
+++ src/main/java/monprojet/dto/PopulationResult.java
@@ -0,0 +1,14 @@
+package monprojet.dto;
+
+import monprojet.dao.CountryRepository;
+
+/**
+ * Un "DTO" (Data Transfer Object) pour représenter 
+ * le résultat d'une requête SQL.
+ * Cette interface sera "automatiquement" implémentée par Spring.
+ * @see monprojet.dao.CountryRepository
+ */
+public interface PopulationResult {
+    String getCountryName();
+    int getPopulationTotale();
+}
